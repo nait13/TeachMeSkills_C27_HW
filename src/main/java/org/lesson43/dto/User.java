5e8f3296e33d029733f17858a16cda5732b084c7
Oleg <naitmaneev@gmail.com>
2024-07-10 14:31:55 +0300

    finished task43

---
+++ src/main/java/org/lesson43/dto/User.java
@@ -0,0 +1,14 @@
+package org.lesson43.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class User {
+    private int id;
+    private String name;
+    private String login;
+}
---
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>ERROR</title>
+</head>
+<body>
+    <h1>ERROR!</h1>
+</body>
+</html>
+
