1f911c48c4462580981ca777dc335cbd1a41c217
Oleg <naitmaneev@gmail.com>
2024-08-12 18:20:51 +0300

    finished task52

--- src/main/java/com/lesson52/entity/Group.java
+++ src/main/java/com/lesson52/entity/Group.java
@@ -1,4 +1,34 @@
 package com.lesson52.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString(exclude = "students")
+@Entity
+@Table(name = "grooup")
+@JsonIgnoreProperties({"students"})
 public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "room")
+    private int room;
+
+    @OneToMany(mappedBy = "group",cascade = CascadeType.ALL)
+    private List<Student> students;
+
 }
--- src/main/java/com/lesson52/entity/Student.java
+++ src/main/java/com/lesson52/entity/Student.java
@@ -1,4 +1,57 @@
 package com.lesson52.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "unpay")
+    private boolean unPay;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return unPay == student.unPay && Objects.equals(id, student.id) && Objects.equals(name, student.name) && Objects.equals(surname, student.surname) && Objects.equals(group, student.group);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, unPay, group);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", unPay=" + unPay +
+                ", group=" + group +
+                '}';
+    }
 }
